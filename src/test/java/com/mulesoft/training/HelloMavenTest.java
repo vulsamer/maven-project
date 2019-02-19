2bc1d36b7eda4142e8436b72e8f3d0f4aa02939d
Volker.Ulsamer <volkergeorg.ulsamer@mt-ag.com>
2019-02-19 09:26:56 +0100

    Addes Validation Test

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Ignore;
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 
 public class HelloMavenTest extends FunctionalTestCase {
@@ -11,6 +12,13 @@ public class HelloMavenTest extends FunctionalTestCase {
     @Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
         runFlowAndExpect("mavenFlow", "Hello Maven");
+    }
+
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+      MuleEvent event = runFlow("retrieveFlights");
+      String contentType = event.getMessage().getOutboundProperty("Content-Type");
+      assertEquals("application/json", contentType);
     }
     
     @Override
